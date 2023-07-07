56901a9c263f87d19df5afb5f65f73aa4518442a
Baha Tarlaci <bahaanil.tarlaci@medyasoft.com.tr>
2023-07-07 23:20:58 +0300

    feat: Html and css structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [datetime, setDatetime] = useState('');
+  const [description, setDescription] = useState('');
+
+  const addNewTransaction = (event) => {
+    event.preventDefault();
+    console.log(name, datetime, description);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form onSubmit={addNewTransaction}>
+        <div className='basic'>
+          <input 
+            type='text' 
+            placeholder={'+200 new Samsung Galaxy S21 5G'}
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input 
+            type='datetime-local'
+            value={datetime}
+            onChange={(event) => setDatetime(event.target.value)}
+          />
+        </div>
+        <div className='description'>
+          <input 
+            type='text' 
+            placeholder={'description'}
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </div>
+        <button type='submit'>Add new transaction</button>
+      </form>
+      <div className="transactions">
+        <div className="transaction">
+          <div className='left'>
+            <div className="name">Samsung Galaxy S21 5G</div>
+            <div className='description'>it's a new phone</div>
+          </div>
+          <div className="right">
+            <div className='price green'>+$200</div>
+            <div className='date'>2021-01-01 20:00</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className='left'>
+            <div className="name">Samsung Galaxy S21 5G</div>
+            <div className='description'>it's a new phone</div>
+          </div>
+          <div className="right">
+            <div className='price red'>-$300</div>
+            <div className='date'>2021-01-01 20:00</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className='left'>
+            <div className="name">Samsung Galaxy S21 5G</div>
+            <div className='description'>it's a new phone</div>
+          </div>
+          <div className="right">
+            <div className='price green'>+$200</div>
+            <div className='date'>2021-01-01 20:00</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
